fbbd12fe1d147a461b01cdc645db8fb4e43994e1
lauradarlak <lauradarlak@gmail.com>
2019-03-23 19:51:31 -0400

    Add Category and ProductsCategory models.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_23_221210) do
+ActiveRecord::Schema.define(version: 2019_03_23_234539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories_products", id: false, force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.bigint "category_id", null: false
+    t.index ["category_id", "product_id"], name: "index_categories_products_on_category_id_and_product_id"
+    t.index ["product_id", "category_id"], name: "index_categories_products_on_product_id_and_category_id"
+  end
 
   create_table "farmers", force: :cascade do |t|
     t.string "name"
