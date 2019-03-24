7c9c8294b0b376f456cbb91f1b90bea9d7fe02d8
lauradarlak <lauradarlak@gmail.com>
2019-03-24 10:07:15 -0400

    Successfully fetch /api/categories.

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react';
 import Categories from './Categories';
-import { Container, Row, Col } from "reactstrap";
+import { Container, Row, Col, Card,
+  Button, CardTitle, CardText } from "reactstrap";
 import './App.css';
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      categories: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/api/categories')
+      .then(response => response.json())
+
+      .then(categories => this.setState({ categories }))
+
+  }
+
   render() {
     return (
       <div>
       <Container>
-        <Row>
-          <Col>
-          Testing App Container!
-          </Col>
-        </Row>
+        <h1>Testing App Container!</h1>
+        <Categories categories={this.state.categories}/>
       </Container>
 
-        <Categories />
+
       </div>
     );
   }
--- client/src/containers/Categories.js
+++ client/src/containers/Categories.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Container, Row, Col, Card,
+  Button, CardTitle, CardText } from "reactstrap";
 
-const Categories = () => (
+const Categories = (props) => (
+
+  <Row>
   <div>
     <h2>Categories Container</h2>
   </div>
+  {props.categories.map(category =>
+    <Col md="4">
+      <Card body>
+        <CardTitle>{category.slug}</CardTitle>
+        <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+        <Button>Go somewhere</Button>
+      </Card>
+    </Col>
+  )}
+  </Row>
 );
 
 
