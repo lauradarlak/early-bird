39a8e90bda450027d7044917be6f08b5f6f648ef
lauradarlak <lauradarlak@gmail.com>
2019-03-25 12:11:18 -0400

    Connected products component to store.

--- client/src/containers/Products.js
+++ client/src/containers/Products.js
@@ -1,45 +1,39 @@
 import React, { Component } from 'react';
-import {fetchProducts } from '../actions/productActions'
+import { connect } from 'react-redux';
+import { fetchProducts } from '../actions/productActions';
 import { Container, Row, Col, Card,
   Button, CardTitle, CardText } from "reactstrap";
 
 export default class Products extends Component {
 
-    state = {
-      productList: []
-    }
-
   componentDidMount() {
     const { category_slug } = this.props.match.params
     this.props.dispatch(getProducts(category_slug))
-
-
   }
 
   render() {
+    const { error, products } = this.props;
     return (
-      <div>
       <Container>
-      <div>
         <h2>Product List Container</h2>
-      </div>
-      {/*
         <Row>
-      {this.state.productList.map(product =>
+        {products.map(product =>
         <Col md="4">
-          <Card body>
+          <Card body key={product.id}>
             <CardTitle>{product.name}</CardTitle>
             <CardText>{product.farmer}</CardText>
           </Card>
         </Col>
-      )}
+        )}
       </Row>
-
-
-
-        */}
       </Container>
-      </div>
     )
   }
 }
+
+const mapStateToProps = state => ({
+  products: state.products.items,
+  error: state.products.erro
+});
+
+export default connect(mapStateToProps)(Products)
