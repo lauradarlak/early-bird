186abd437c85de1002f36d274c9fe13cacee1556
lauradarlak <lauradarlak@gmail.com>
2019-03-28 10:07:18 -0400

    Modified API routes and controller to receive PATCH request.

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,9 +1,9 @@
 class Api::ProductsController < ApplicationController
 
-  # def show
-  #   product = Product.find_by(id: params[:id])
-  #   render json: product
-  # end
+  def show
+    product = Product.find_by(id: params[:id])
+    render json: product
+  end
 
   def update
     product = Product.find_by(id: params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  
+
   namespace :api do
     resources :categories, only: [:index, :show], param: :slug
+    resources :products, only: [:show, :update]
   end
 end
