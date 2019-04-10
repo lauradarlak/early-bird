671e3abf596221d9d77b6da84e2142391716e0e5
lauradarlak <lauradarlak@gmail.com>
2019-04-10 00:12:08 -0400

    Extend Order component.

--- client/src/components/Order.js
+++ client/src/components/Order.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Alert, Container } from 'reactstrap';
 
-const Order = () => (
-  <Container className="mt-4">
-    <Alert className="w-50 mx-auto" color="success">
-      <h2 className="alert-heading">Thank you for ordering!</h2>
-      <p>
-        We will let the farmer's know that you have made an order. Your order will be ready for pickup at the Saturday morning farmer's market.
-      </p>
-    </Alert>
-  </Container>
-)
-
-export default Order
+export default class Order extends Component {
+  render() {
+    return(
+      <Container className="mt-4">
+        <Alert className="w-50 mx-auto" color="success">
+          <h2 className="alert-heading">Thank you for ordering!</h2>
+          <p>
+            We will let the farmer's know that you have made an order. Your order will be ready for pickup at the Saturday morning farmer's market.
+          </p>
+        </Alert>
+      </Container>
+    )
+  }
+}
