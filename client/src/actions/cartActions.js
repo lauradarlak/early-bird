c177a5f3f1bea778ef9d2e7114576295b446103a
lauradarlak <lauradarlak@gmail.com>
2019-04-11 12:46:06 -0400

    Added alert to successful order.

--- client/src/actions/cartActions.js
+++ client/src/actions/cartActions.js
@@ -14,14 +14,16 @@ export const addToCart = (item, slug)  => ({
 
 export const updateInventory = (item) => {
 
-  fetch(`api/products/${item.id}`, {
+  fetch(`http://localhost:3001/api/products/${item.id}`, {
     headers: { 'Content-Type': 'application/json' },
     method: 'PATCH',
     body: JSON.stringify({
       quantity: item.quantity})
   })
-  .then(res => console.log(res))
-  .then(window.location.pathname = '/order')
+  .then((res) => {
+    
+    console.log(res);
+  })
 
   .catch(console.error)
 
--- client/src/components/OrderButton.js
+++ client/src/components/OrderButton.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import SweetAlert from 'react-bootstrap-sweetalert';
 import { updateInventory, resetCart } from '../actions/cartActions';
 import { Button } from "reactstrap";
 
-  const OrderButton = ({ hasProducts, cart, resetCart }) => {
+class OrderButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      orderComplete: false
+    }
+  }
+
+  hideAlert = () => {
+    this.setState({
+        orderComplete: false
+    });
+  }
+
+  render() {
+    const { hasProducts, cart, resetCart } = this.props;
     const handleOnSubmit = event => {
       const orderArray = cart;
       console.log('A')
@@ -12,17 +28,25 @@ import { Button } from "reactstrap";
         return updateInventory(item)
       })
       console.log('B')
+      this.setState({ orderComplete: true });
       resetCart();
     }
 
-    return (
+    return(
+      <>
       <Button
         className="mt-3 d-inline"
         disabled={hasProducts ? false : true}
         color="success" onClick={() => handleOnSubmit()}
         type="submit">Send Order</Button>
+        <div>
+      {this.state.orderComplete === false ?
+        "" : <SweetAlert success title="Successful Order!" onConfirm={this.hideAlert.bind(this)}>We will let the farmers know about your order. Your order will be ready for pickup at the Saturday morning farmers market. </SweetAlert>
+      }
+      </div>
+      </>
     )
-
   }
+}
 
 export default withRouter(connect(null, { resetCart })(OrderButton))
